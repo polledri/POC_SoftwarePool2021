05de392862f304b222c4e263b8d385e3e17f9ae0
pantoine <antoine.polledri@epitech.eu>
2021-02-16 19:24:06 +0100

    ex07

--- day02/src/server.ts
+++ day02/src/server.ts
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 import {PORT, HOST, HELLO_MESSAGE } from './serverConfig';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
-ReasonPhrases.BAD_REQUEST
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -16,6 +15,7 @@ app.get('/hello', (req, res) => {
     }
     res.send(HELLO_MESSAGE)
 });
+
 app.get('/repeat-my-query', (req, res) => {
     if (req.query.message === undefined) {
         res.status(StatusCodes.BAD_REQUEST).send('400 '+ReasonPhrases.BAD_REQUEST);
@@ -24,11 +24,13 @@ app.get('/repeat-my-query', (req, res) => {
     res.send('Response send to client: '+req.query.message);
     return req.query.message;
 })
+
 app.get('/repeat-my-param/:message', (req, res) => {
     const param = req.params.message
     res.send('Response send to client: '+param);
     return param;
 })
+
 app.post('/repeat-my-body',  (req, res) => {
     if (req.body === undefined) {
         res.status(StatusCodes.BAD_REQUEST).send('400 '+ReasonPhrases.BAD_REQUEST);
@@ -37,6 +39,7 @@ app.post('/repeat-my-body',  (req, res) => {
     res.send(req.body);
     return req.body;
 })
+
 app.get('/repeat-my-header', (req, res) => {
     var head = req.header('X-Message');
     if (head === undefined) {
@@ -46,6 +49,7 @@ app.get('/repeat-my-header', (req, res) => {
     res.send('header: '+head);
     return head;
 })
+
 app.get('/repeat-my-cookie', (req, res) => {
     var cookie = req.cookies;
     if (cookie === undefined) {
@@ -55,8 +59,48 @@ app.get('/repeat-my-cookie', (req, res) => {
     res.send(cookie);
     return cookie
 })
+
 app.get('/health', (req, res) => {
     res.status(StatusCodes.OK).send('200 '+ReasonPhrases.OK);
+})
+
+app.get('/repeat-all-my-queries', (req, res) => {
+    const keys = Object.keys(req.query);
+    if (!keys.length) {
+        res.status(StatusCodes.BAD_REQUEST).send('400 '+ReasonPhrases.BAD_REQUEST);
+        return;
+    }
+    let tab: Object[] = [];
+    keys.forEach(item => {
+        tab.push({ key : item, value : req.query[item] as string })
+    })
+    res.status(StatusCodes.OK).send(tab);
+    return tab;
+})
+
+function isPalindrome(str: string) : boolean {
+    let tmp = str.toLowerCase().trim();
+    if (!tmp.length || tmp.length == 1)
+        return true;
+    if (tmp.charAt(0).localeCompare(tmp.charAt(tmp.length - 1)) == 0) {
+        let tmp2 = tmp.slice(1, tmp.length - 1)
+        return isPalindrome(tmp2);
+    }
+    return false;
+}
+
+app.post('/are-these-palindromes',  (req, res) => {
+    const palin = Object.assign(req.body);
+    if (!palin.length) {
+        res.status(StatusCodes.BAD_REQUEST).send('400 '+ReasonPhrases.BAD_REQUEST);
+        return;
+    }
+    let tab: Object[] = [];
+    palin.forEach((item: string) => {
+        tab.push({ input : item, result : isPalindrome(item) })
+    })
+    res.status(StatusCodes.OK).send(tab);
+    return tab;
 })
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
